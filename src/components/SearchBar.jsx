ed41df5137449d7929c83f26d8441e7334526f92
Lula-Benni <lulabenni45@gmail.com>
2024-06-28 21:01:19 +0200

    Made the category and search buttons to be transparent

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,8 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function SearchBar(){
     return(
-        <div className="px-2 bg-slate-100 rounded-full flex flex-wrap box-border py-1">
-            <select name='Category' className="bg-slate-100 categories w-28 px-2" id="categories">
+        <div className="px-2 bg-slate-100 rounded-full flex flex-wrap box-border max-w-fit py-1">
+            <select name='Category' className="px-2 bg-transparent w-28" id="categories">
                 <option value="" disabled selected>Category</option>
                 <option value="CELLPHONE">Cellphone</option>
                 <option value="ACCESSORIES">Accessories</option>
@@ -13,8 +13,8 @@ function SearchBar(){
                 <option value="MUSICAL_INSTRUMENTS">Musical Instruments</option>
                 <option value="BOOKS">Books</option>
             </select>
-            <input type="text" className="border-l-2 border-r-2 bg-slate-100 search w-64 h-auto px-2" id="search" placeholder="What are you looking for?"/>
-            <button className='fa-solid fa-search bg-slate-100 px-2'><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
+            <input type="text" className="pl-2 w-64 bg-transparent" id="search" placeholder="What are you looking for?"/>
+            <button className='size-fit bg-transparent'><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
         </div>
     )
 }
