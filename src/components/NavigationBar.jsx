628363a755cc7a5b7839248a636f830e17c346f5
Lula-Benni <lulabenni45@gmail.com>
2024-06-28 12:43:48 +0200

    Made a component for SearchBar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,3 +1,4 @@
+import SearchBar from './SearchBar'
 function NavigationBar(){
     return(
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -6,22 +7,17 @@ function NavigationBar(){
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
                 <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="input-container flex flex-wrap box-border h-10">
-                    <input type="text" className="categories w-24" id="categories" placeholder="Categories"/>
-                    <input type="text" className="search w-64" id="search" placeholder="What are you looking for?"/>
-                    <button className='bg-white'>Search</button>
-                </div>
+                <SearchBar/>
                 <div className="flex flex-wrap">
-                <ul className="navbar-nav me-auto">
-                    <li className="nav-item">
-                        <a className="nav-link" href="javascript:void(0)">Link</a>
-                    </li>
-                    
-                </ul>
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="javascript:void(0)">Link</a>
+                        </li>
+                        
+                    </ul>
                 </div>
             </div>
         </nav>
-
     )
 }
 export default NavigationBar;
