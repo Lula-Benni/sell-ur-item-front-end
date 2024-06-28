94f2a1d645a5403b0a03d3c534f32309fc557177
Lula-Benni <lulabenni45@gmail.com>
2024-06-28 21:03:15 +0200

    Made styling and added icon to the app name

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,19 +1,32 @@
 import SearchBar from './SearchBar'
+import HamburgerMenu from './HamburgerMenu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import icon from '../assets/icon.png'
+
 function NavigationBar(){
     return(
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-black">
             <div className="container-fluid">
-                <a className="navbar-brand" href="javascript:void(0)">Logo</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <SearchBar/>
-                <div className="flex flex-wrap">
-                    <ul className="navbar-nav me-auto">
+                <a className="no-underline flex items-center w-12 h-9 text-white" href="javascript:void(0)">
+                    <img className="icon" src={icon} alt="icon"/>
+                    <h4 className="nav-heading">SellUrItem</h4>
+                </a>
+                <SearchBar className='size-3/5'/>
+                <HamburgerMenu/>
+                <div className="hidden md:flex flex-wrap">
+                    <ul className="space-x-20 navbar-nav m-auto">
                         <li className="nav-item">
-                            <a className="nav-link" href="javascript:void(0)">Link</a>
+                            <a className="nav-link" href="javascript:void(0)"><FontAwesomeIcon className='text-white' icon={faBookmark} /></a>
                         </li>
-                        
+                        <li className="nav-item">
+                            <a className="nav-link" href="javascript:void(0)"><FontAwesomeIcon className='text-white' icon={faComment} /></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="javascript:void(0)"><FontAwesomeIcon className='text-white' icon={faUser} /></a>
+                        </li> 
                     </ul>
                 </div>
             </div>
