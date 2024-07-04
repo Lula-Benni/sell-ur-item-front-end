922b125333f21f3d0d1c64e2b830e0ee43de31eb
Lula-Benni <lulabenni45@gmail.com>
2024-07-04 13:00:17 +0200

    Styling SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,8 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function SearchBar(){
     return(
-        <div className="px-2 bg-slate-100 rounded-full flex flex-wrap box-border max-w-fit py-1">
-            <select name='Category' className="px-2 bg-transparent w-28" id="categories">
+        <div className="px-2 bg-slate-100 rounded-full inline py-1">
+            <select name='Category' className="hidden custom-sm:inline px-2 bg-transparent w-28" id="categories">
                 <option value="" disabled selected>Category</option>
                 <option value="CELLPHONE">Cellphone</option>
                 <option value="ACCESSORIES">Accessories</option>
@@ -13,7 +13,7 @@ function SearchBar(){
                 <option value="MUSICAL_INSTRUMENTS">Musical Instruments</option>
                 <option value="BOOKS">Books</option>
             </select>
-            <input type="text" className="pl-2 w-64 bg-transparent" id="search" placeholder="What are you looking for?"/>
+            <input type="text" className="pl-2 bg-transparent" style={{width: '210px'}} id="search" placeholder="What are you looking for?"/>
             <button className='size-fit bg-transparent'><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
         </div>
     )
