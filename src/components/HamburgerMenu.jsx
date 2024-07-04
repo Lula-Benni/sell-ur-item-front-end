8aa8b9f67da8a340ad2d4d37f9e5202f5c500788
Lula-Benni <lulabenni45@gmail.com>
2024-07-04 13:03:29 +0200

    Fixed the Hamburger btn menu

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -13,22 +13,14 @@ const HamburgerMenu = () => {
   };
 
   return (
-    <div>
-      <button className='md:hidden' onClick={toggleMenu} type="button">
+    <div className='pr-10'>
+      <button className='custom-md:hidden' onClick={toggleMenu} type="button">
         <span><FontAwesomeIcon className="text-white" icon={faBars} /></span>
       </button>
-      <div className={`mt-2 ${isOpen ? 'block' : 'hidden'}`}>
-        <ul className="space-x-20 navbar-nav">
-            <li className="nav-item">
-                <a className="nav-link" href="javascript:void(0)"><FontAwesomeIcon className='text-white' icon={faBookmark} /></a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="javascript:void(0)"><FontAwesomeIcon className='text-white' icon={faComment} /></a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="javascript:void(0)"><FontAwesomeIcon className='text-white' icon={faUser} /></a>
-            </li> 
-        </ul>
+      <div className={`text-black whitespace-nowrap text-center shadow-[0_8px_16px_0px_rgba(0,0,0,0.2)] bg-blend-darken bg-white-smoke absolute top-12 right-2 ${isOpen ? 'block' : 'hidden'}`}>
+        <a className="py-1 px-4 hover:bg-footer-text-col nav-link" href="javascript:void(0)">Go to Profile</a>
+        <a className="py-1 px-4 hover:bg-footer-text-col nav-link" href="javascript:void(0)">Messages</a>
+        <a className="py-1 hover:bg-footer-text-col px-4 nav-link" href="javascript:void(0)">Saved Items</a>
     </div>
     </div>
   )
