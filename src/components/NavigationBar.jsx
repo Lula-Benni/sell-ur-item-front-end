8df1adb1f1bde8f273ed940f8841b0f8cb2c7da1
Lula-Benni <lulabenni45@gmail.com>
2024-07-04 13:01:48 +0200

    Adding padding to the NavBar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -9,15 +9,17 @@ import icon from '../assets/icon.png'
 function NavigationBar(){
     return(
         <nav className="navbar navbar-expand-sm navbar-dark bg-black">
-            <div className="container-fluid">
-                <a className="no-underline flex items-center w-12 h-9 text-white" href="javascript:void(0)">
-                    <img className="icon" src={icon} alt="icon"/>
-                    <h4 className="nav-heading">SellUrItem</h4>
-                </a>
-                <SearchBar className='size-3/5'/>
+            <div className="w-full flex justify-between items-center">
+                <div className='hidden sm:flex flex items-center'  style={{paddingRight: '20%'}} >
+                    <a className="no-underline flex items-center w-12 h-9 text-white" href="javascript:void(0)">
+                        <img className="icon" src={icon} alt="icon"/>
+                        <h4 className="navbar-brand">SellUrItem</h4>
+                    </a>
+                </div>
+                <SearchBar className='inline'/>
                 <HamburgerMenu/>
-                <div className="hidden md:flex flex-wrap">
-                    <ul className="space-x-20 navbar-nav m-auto">
+                <div  style={{paddingLeft: '2%', paddingRight: '3%'}} className="hidden custom-md:flex items-center">
+                    <ul className="inline items-end justify-end space-x-10 navbar-nav">
                         <li className="nav-item">
                             <a className="nav-link" href="javascript:void(0)"><FontAwesomeIcon className='text-white' icon={faBookmark} /></a>
                         </li>
