d93ff05cc67b66562dc5dea836347c52f1fdb270
Lula-Benni <lulabenni45@gmail.com>
2024-07-11 01:43:23 +0200

    Added a drop down menu for the user/profile view button

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,12 +1,18 @@
+import React, { useState } from 'react';
 import SearchBar from './SearchBar'
 import HamburgerMenu from './HamburgerMenu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faComment } from '@fortawesome/free-solid-svg-icons';
 import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import icon from '../assets/icon.png'
 
 function NavigationBar(){
+    const [show, setShow] = useState(false);
+
+    const toggleDropdown = () => setShow(!show);
+  
     return(
         <nav className="navbar navbar-expand-sm navbar-dark bg-black">
             <div className="w-full flex justify-between items-center">
@@ -27,7 +33,21 @@ function NavigationBar(){
                             <a className="nav-link" href="javascript:void(0)"><FontAwesomeIcon className='text-white' icon={faComment} /></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="javascript:void(0)"><FontAwesomeIcon className='text-white' icon={faUser} /></a>
+                            <a id='dropdown-menu' 
+                               className="nav-link dropdown-toggle" 
+                               href='#'
+                               data-bs-toggle="dropdown"
+                               aria-expanded={show}
+                               onClick={toggleDropdown}
+                               >
+                                <FontAwesomeIcon className='text-white' icon={faUser}></FontAwesomeIcon></a>
+                            <div className={` w-fit whitespace-nowrap text-center dropdown-menu dropdown-menu-end ${show ? 'show' : ''}`} 
+                                aria-labelledby="dropdown-menu">
+                                <a className="py-1 text-black px-4 hover:bg-footer-text-col nav-link" href="javascript:void(0)">Go to Profile</a>
+                                <a className="py-1 text-black px-4 hover:bg-footer-text-col nav-link" href="javascript:void(0)">My Items</a>
+                                <a className="py-1 text-black hover:bg-footer-text-col px-4 nav-link" href="javascript:void(0)">Add Items</a>
+                                <a className="py-1 text-black px-4 hover:bg-footer-text-col nav-link" href="javascript:void(0)">Log Out</a>
+                            </div>
                         </li> 
                     </ul>
                 </div>
