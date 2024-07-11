ed495741f0b9c2163509942ba269ffe2b7b852e1
Lula-Benni <lulabenni45@gmail.com>
2024-07-11 02:22:26 +0200

    Changed text color and removed required for remember me

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,19 +2,19 @@ const Login = () =>{
     return(
         <div style={{marginLeft: '36%'}} className="my-4 max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
             <form className="space-y-6" action="#">
-                <h5 className="text-xl font-medium text-black dark:text-white">Sign in to our platform</h5>
+                <h5 className="text-xl font-medium text-black dark:text-white">Sign in to SellUrItem</h5>
                 <div>
-                    <label for="email" className="block mb-2 text-sm font-medium text-black dark:text-white">Enter your email</label>
-                    <input type="email" name="email" id="email" className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                    <label for="username" className="block mb-2 text-sm font-medium text-black dark:text-white">Enter your username</label>
+                    <input type="text" name="username" id="username" className="bg-white border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="username" required />
                 </div>
                 <div>
                     <label for="password" className="block mb-2 text-sm font-medium text-black dark:text-white">Enter your password</label>
-                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-white border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
                 </div>
                 <div className="flex items-start">
                     <div className="flex items-start">
                         <div className="flex items-center h-5">
-                            <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                            <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
                         </div>
                         <label for="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
                     </div>
