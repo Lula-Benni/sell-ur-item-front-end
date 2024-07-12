192a05d3b35326e7d5f13816b1aa20572df71fe8
Lula-Benni <lulabenni45@gmail.com>
2024-07-12 02:02:22 +0200

    Added states for linking the login, register and forgot password achors/links

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,26 @@ import NavigationBar from './components/NavigationBar'
 import Footer from './components/FooterBar'
 import FooterBar from './components/FooterBar'
 import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
+import ForgotPassword from './components/ForgotPassword'
 function App() {
   const [count, setCount] = useState(0)
+
+  const [view, setView] = useState('login');
+
+  const showRegister = () => setView('register');
+  const showLogin = () => setView('login');
+  const showForgotPassword = () => setView('forgotPassword');
 
   return (
     <div className='App'>
       <NavigationBar/>
-      <div className='p-2.5'><Home/></div>
-      
+        {view==='login' ?
+          <Login showRegister={showRegister} showForgotPassword={showForgotPassword} />
+          : view==='register' ? <Register showLogin={showLogin} />: 
+          <ForgotPassword showLogin={showLogin} />
+        }
       <FooterBar/>
     </div>
   )
