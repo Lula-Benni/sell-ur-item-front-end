11ebedfb272f61824fc6e32bb3435114d1d11df8
Lula-Benni <lulabenni45@gmail.com>
2024-07-16 00:49:30 +0200

    Adding forms for Edit components

--- src/components/edit-components/EditDescription.jsx
+++ src/components/edit-components/EditDescription.jsx
@@ -11,6 +11,7 @@ const EditDescription = () =>{
     };
     return(
         <div className=''>
+            <form>
             <a id='dropdown-menu' 
                 style={{position: 'relative', bottom: '69px', left: '75%'}}
                 href='#'
@@ -26,6 +27,7 @@ const EditDescription = () =>{
                     <button style={{width: '100%'}} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' type="submit" id="save-item-price" name="save-item-name">Save</button>
                 </div>
             </div>
+            </form>
         </div>
     )
 }
