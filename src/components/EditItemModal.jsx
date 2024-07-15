2bd3223c77ac9e0015e813d3d5efa8865478ad99
Lula-Benni <lulabenni45@gmail.com>
2024-07-15 14:05:55 +0200

    Added body part in the Edit iem modal

--- src/components/EditItemModal.jsx
+++ src/components/EditItemModal.jsx
@@ -3,6 +3,9 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen } from '@fortawesome/free-solid-svg-icons';
+import EditPrice from './edit-components/EditPrice';
+import EditName from './edit-components/EditName';
+import EditDescription from './edit-components/EditDescription';
 
 function EditItemModal() {
   const [show, setShow] = useState(false);
@@ -24,10 +27,39 @@ function EditItemModal() {
           <Modal.Title>Seller Information</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Are you sure you want to delete this Item?</p>
+          <div>
+            <div>
+              <h5>Item Name</h5>
+              <div className='pt-2' >
+                <h6 >Iphone 7</h6>
+              </div>
+              <EditName/>
+            </div>
+            <hr/>
+            <div>
+              <h5>Item Price</h5>
+              <div className='pt-2'>
+                <h6 className=''>R3000.000</h6>       
+              </div>
+              <EditPrice/>
+            </div>
+            <hr/>
+            <div className='flex flex-row space-x-52'>
+              <h5>Item Category</h5>
+            </div>
+            <div className='pt-2' >
+              <h6 >John Doe</h6>
+            </div>
+            <hr/>
+            <div>
+              <h5>Item Description</h5>
+              <div className='pt-2' >
+                <h6 >Black</h6>
+              </div>
+              <EditDescription/>
+            </div>
+          </div>
         </Modal.Body>
-        <Modal.Footer>
-        </Modal.Footer>
       </Modal>
     </>
   );
