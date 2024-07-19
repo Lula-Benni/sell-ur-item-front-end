f65c3eb84b5eb5a385c222ce76c8d6df38bd03a2
Lula-Benni <lulabenni45@gmail.com>
2024-07-19 22:25:32 +0200

    Added from and to messages components

---
+++ src/components/messages/FromMessages.jsx
@@ -0,0 +1,10 @@
+const FromMessage = () => {
+    return(
+        <div className="flex">
+            <div className="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
+                Not too bad, just a bit busy. How about you?
+            </div>
+        </div>
+    )
+}
+export default FromMessage
---
+++ src/components/messages/ToMessage.jsx
@@ -0,0 +1,10 @@
+const ToMessage = () =>{
+    return(
+        <div className="flex justify-end">
+            <div className="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
+                Hey, how's your day going?
+            </div>
+        </div>
+    )
+} 
+export default ToMessage
